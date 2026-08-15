427237e12b23f59890743eee0efc19fc6d647bc5
hunainahseerat <hunainahseerat3344@gmail.com>
2026-08-15 17:27:15 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  typescript: {
+    // Allows production builds to complete successfully even if
+    // your project has TypeScript type errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
